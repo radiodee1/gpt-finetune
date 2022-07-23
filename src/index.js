2540992a28d8edc4e067b181be8973d5990cfaa0
David Liebman <radiodee1@gmail.com>
2022-07-23 14:06:39 -0400

    json.stringify better

--- src/index.js
+++ src/index.js
@@ -77,17 +77,17 @@ function write_corpus(text, index_1, i) {
     console.log("complete");
   });
 
-  var jtext = {"text":`${text}\n${text_out[i]}${index_1}\n\n`, "meta":{"pile_set_name":"Count"}};
+  var jtext = {"text":`${text}\n${text_out[i]}${index_1}\n`, "meta":{"pile_set_name":"Count"}};
   //jtext = jtext.replace("/\n/g", "\u000D");
   jtext = JSON.stringify(jtext);
-
+  /*
   if (jtext.startsWith("\"")) {
     jtext = jtext.slice(1);
   }
   if (jtext.endsWith("\"")) {
     jtext = jtext.slice(0, jtext.length - 1);
   }
-  
+  */ 
   //jtext = jtext.replace(`/\\\"/g`,`"`);
 
   console.log(jtext);
