43217c65ce91a45bee441cd1063a4f561347b94f
David Liebman <radiodee1@gmail.com>
2022-07-28 15:45:56 -0400

    train takes 2 plus minutes

---
+++ train/train.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+
+from happytransformer import HappyGeneration, GENTrainArgs
+#--------------------------------------#
+
+happy_gen = HappyGeneration( model_type="GPT-NEO", model_name="EleutherAI/gpt-neo-125M", load_path="../model/" )  # default uses gpt2
+#args = GENSettings(max_length=15)
+args = GENTrainArgs(num_train_epochs=1) 
+
+#result = happy_gen.generate_text("artificial intelligence is ", args=args)    
+#print(result)  # GenerationResult(text='\xa0a new field of research that has been gaining momentum in recent years.')
+#print(result.text)  #  a new field of research that has been gaining momentum in recent years.
+
+happy_gen.train("../src/corpus.00.txt", args=args);
+
+happy_gen.save("../model/")
