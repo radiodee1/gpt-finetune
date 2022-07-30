ccf9e0f86d5514d87cb937f8bd9f55dacc1a6ff3
David Liebman <radiodee1@gmail.com>
2022-07-30 16:40:48 -0400

    set to 30 epochs

--- train/train.py
+++ train/train.py
@@ -9,7 +9,7 @@ print(path)
 
 happy_gen = HappyGeneration( model_type="GPT-NEO", model_name="EleutherAI/gpt-neo-125M", load_path=path )  # default uses gpt2
 #args = GENSettings(max_length=15)
-args = GENTrainArgs(num_train_epochs=10) 
+args = GENTrainArgs(num_train_epochs=30) 
 
 #result = happy_gen.generate_text("artificial intelligence is ", args=args)    
 #print(result)  # GenerationResult(text='\xa0a new field of research that has been gaining momentum in recent years.')
