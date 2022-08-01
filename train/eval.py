c66a8549511bbbff8ea404171680cdeb8b06c882
David Liebman <radiodee1@gmail.com>
2022-08-01 09:56:22 -0400

    eval

--- train/eval.py
+++ train/eval.py
@@ -7,7 +7,7 @@ path = str(os.environ.get("GPT_ETC_CHECKPOINT"))
 print(path)
 
 happy_gen = HappyGeneration(load_path=path)  
-args = GENEvalArgs(preprocessing_processes=2)
+args = GENEvalArgs(preprocessing_processes=2) #, save_preprocessed_data=True, save_preprocessed_data_path="../data/corpus.json")
 result = happy_gen.eval("../src/corpus.00.txt", args=args)
 print(type(result))  # <class 'happytransformer.happy_trainer.EvalResult'>
 print(result)  # EvalResult(loss=3.3437771797180176)
